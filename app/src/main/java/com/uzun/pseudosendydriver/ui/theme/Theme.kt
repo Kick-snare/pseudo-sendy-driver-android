e382d2ec0f27fba092a5af36fab2758f6e71ebf8
uzun <hyjhyj0901@pusan.ac.kr>
2023-03-13 09:49:49 +0900

    [chore] 폰트, 서체, 색상, 테마 추가

--- app/src/main/java/com/uzun/pseudosendydriver/ui/theme/Theme.kt
+++ app/src/main/java/com/uzun/pseudosendydriver/ui/theme/Theme.kt
@@ -5,6 +5,8 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.staticCompositionLocalOf
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -16,32 +18,28 @@ private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
     secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
 )
 
-@Composable
-fun PseudoSendyDriverTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+val localSendyTypography = staticCompositionLocalOf { sendyTypography }
 
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+@Composable
+fun PseudoSendyTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+
+    val colors = if (false) DarkColorPalette else LightColorPalette
+
+    CompositionLocalProvider(
+        localSendyTypography provides SendyTypography()
+    ) {
+        MaterialTheme(
+            colors = colors,
+            shapes = Shapes,
+            content = content
+        )
+    }
+}
+
+object PseudoSendyTheme {
+    val typography: SendyTypography
+        @Composable
+        get() = localSendyTypography.current
 }
--- app/src/main/java/com/uzun/pseudosendydriver/ui/theme/Type.kt
+++ app/src/main/java/com/uzun/pseudosendydriver/ui/theme/Type.kt
@@ -1,28 +1,58 @@
 package com.uzun.pseudosendydriver.ui.theme
 
-import androidx.compose.material.Typography
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
+import com.uzun.pseudosendydriver.R
+import javax.annotation.concurrent.Immutable
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+val NotoSansKR = FontFamily(
+    Font(R.font.notosanskr_black, FontWeight.Black, FontStyle.Normal),
+    Font(R.font.notosanskr_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.notosanskr_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.notosanskr_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.notosanskr_light, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.notosanskr_thin, FontWeight.Thin, FontStyle.Normal),
 )
+
+fun getNotoSansTextStyle(
+    fontSize : TextUnit,
+    weight: FontWeight = FontWeight.Normal,
+    color: Color = DayGrayscale100
+) = TextStyle(
+    fontFamily = NotoSansKR,
+    fontWeight = weight,
+    fontSize = fontSize,
+    platformStyle = PlatformTextStyle(includeFontPadding = false),
+    color = color
+)
+
+@Immutable
+data class SendyTypography(
+    val XXXLBold : TextStyle = getNotoSansTextStyle(32.sp, FontWeight.Bold),
+    val XXXL : TextStyle = getNotoSansTextStyle(32.sp),
+    val XXLBold : TextStyle = getNotoSansTextStyle(28.sp, FontWeight.Bold),
+    val XXL : TextStyle = getNotoSansTextStyle(28.sp),
+    val XLBold : TextStyle = getNotoSansTextStyle(24.sp, FontWeight.Bold),
+    val XL : TextStyle = getNotoSansTextStyle(24.sp),
+    val LargeBold : TextStyle = getNotoSansTextStyle(20.sp, FontWeight.Bold),
+    val Large : TextStyle = getNotoSansTextStyle(20.sp),
+    val MediumBold : TextStyle = getNotoSansTextStyle(18.sp, FontWeight.Bold),
+    val Medium : TextStyle = getNotoSansTextStyle(18.sp),
+    val NormalBold : TextStyle = getNotoSansTextStyle(16.sp, FontWeight.Bold),
+    val Normal : TextStyle = getNotoSansTextStyle(16.sp),
+    val SmallBold : TextStyle = getNotoSansTextStyle(14.sp, FontWeight.Bold),
+    val Small : TextStyle = getNotoSansTextStyle(14.sp),
+    val XSBold : TextStyle = getNotoSansTextStyle(13.sp, FontWeight.Bold),
+    val XS : TextStyle = getNotoSansTextStyle(13.sp),
+    val XXSBold : TextStyle = getNotoSansTextStyle(12.sp, FontWeight.Bold),
+    val XXS : TextStyle = getNotoSansTextStyle(12.sp),
+)
+
+val sendyTypography = SendyTypography()
