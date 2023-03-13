57ccbcb65193f4611c2190c623d11431f85b0aab
uzun <hyjhyj0901@pusan.ac.kr>
2023-03-13 11:32:41 +0900

    [chore] UIConst 디자인 상수값 추가

---
+++ app/src/main/java/com/uzun/pseudosendydriver/presentation/_const/UIConst.kt
@@ -0,0 +1,25 @@
+package com.uzun.pseudosendydriver.presentation._const
+
+import androidx.compose.ui.unit.dp
+
+object UIConst {
+    // spacing
+    val SPACE_XXS = 4.dp
+    val SPACE_XS = 8.dp
+    val SPACE_S = 12.dp
+    val SPACE_M = 16.dp
+    val SPACE_L = 20.dp
+    val SPACE_XL = 24.dp
+    val SPACE_XXL = 32.dp
+
+    // button
+    val BUTTON_WIDTH_LARGE = 360.dp
+    val BUTTON_WIDTH_NORMAL = 280.dp
+    val BUTTON_WIDTH_SMALL = 232.dp
+    val BUTTON_HEIGHT_LARGE = 30.dp
+    val BUTTON_HEIGHT_NORMAL = 27.dp
+    val BUTTON_HEIGHT_SMALL = 24.dp
+    val BUTTON_RADIUS_LARGE = 12.dp
+    val BUTTON_RADIUS_NORMAL = 8.dp
+    val BUTTON_RADIUS_SMALL = 6.dp
+}
